bbfdcaa9c17aacd0ac6d2c77286ba59035a52396
codekushall <shresthakushal913@gmail.com>
2026-02-17 22:36:43 +0545

    variable let and const

---
+++ 3_variables/c_let_and_const_variable.js
@@ -0,0 +1,15 @@
+// Variables are declared to store data that we plan to use throughout out program.
+// We use both let and const variable to declare variables as its is preferred ways to decalre variables. but these both differ in how they assign and ressign the value.
+
+
+let age = 30;
+console.log(age); //30
+age = 80;
+console.log(age); //80
+// Here first the value of age is 10 and in second age is assigned the value 80 as the program runs let makes it useful to change the values as the program runs. We can change the value as per our need which let helps us to do.
+
+
+const score = 200;
+console.log(score); // this displays 200
+score = 300;
+console.log(score); // this shows error
