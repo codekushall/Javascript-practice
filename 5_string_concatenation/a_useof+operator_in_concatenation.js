09616e188f846a65190e81fad865ebe956c742c2
codekushall <shresthakushal913@gmail.com>
2026-02-18 22:35:42 +0545

    concatenation

---
+++ 5_string_concatenation/a_useof+operator_in_concatenation.js
@@ -0,0 +1,9 @@
+let firstName = "Kushal";
+let lastName = "Shrestha";
+
+let fullName = firstName + " " + lastName;
+console.log(fullName);
+// console.log(firstName + " " + lastName); this method can also be used to print fullname
+
+
+// Here + operator is used to combine the firstName and lastName,it is string concatenation.And "" creates sapce betwwen firstName and lastName.
