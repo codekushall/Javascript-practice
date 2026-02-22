918fec9e677d02655d6d0e7026027c4c7d397a18
codekushall <shresthakushal913@gmail.com>
2026-02-22 21:28:46 +0545

    string concatenation

---
+++ 5_string_concatenation/b_use_of_+=_operator.js
@@ -0,0 +1,4 @@
+let greeting = "Hello";
+greeting += " Kushal"; // Here += operator is used to add data in existing string.
+
+console.log(greeting); // Hello Kushal
---
+++ 5_string_concatenation/c_string_concat.js
@@ -0,0 +1,8 @@
+let firstString = "Sanothimi";
+let secondString = "Campus";
+let thirdString = "ICT"
+
+let college = firstString.concat(' '+ secondString + ' ' + thirdString);
+console.log(college);
+
+// with concatenation we can join different strings together to form a single string
